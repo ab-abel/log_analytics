7bb56c9a01973acdd868aa7504eeb4fe6463d52d
abel <ab.bell205@gmail.com>
2024-01-05 23:13:23 +0100

    modify registration controller

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session,request, flash, redirect, url_for
 import os
+
+from app.model import LoginForm, RegisterForm, User, db
 
 # dotenv loader
 from dotenv import load_dotenv
@@ -14,9 +16,42 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 def home():
     return render_template('auth/login.html')
 
-@app.route('/register')
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('auth/register.html')
+    if session.get('username'):
+        flash('You are already logged in', 'info')
+        return redirect(url_for('login'))
+    
+    form = RegisterForm()
+    if form.validate_on_submit():
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        password = request.form.get('password') 
+        email = request.form.get('email')
+        existing_email = User.query.filter(
+            User.email.like('%' + email + '%')).first()
+        if existing_email:
+            flash (
+                'this user email is already registered', 'warning'
+            )
+        user = User(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            password=password
+            )
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now register. Please login', 'succcess')
+        
+        return redirect(url_for('login'))
+    if form.errors:
+        flash(form.error, 'danger')
+    return render_template('auth/register.html', form=form)
 
 @app.route('/dashboard')
 def dashboard():
