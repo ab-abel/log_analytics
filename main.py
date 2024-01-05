24024582480cf3a137a748c07093782cc63bf229
abel <ab.bell205@gmail.com>
2024-01-05 18:41:43 +0100

    added enviromet config

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import os
+
+# dotenv loader
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
