475e07535f2dd8a1163f70b08fb3788fa7a9b8d6
abel <ab.bell205@gmail.com>
2024-01-06 18:41:48 +0100

    added login using login manager

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 from app.model import LoginForm, RegisterForm, User, db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# login 
+from flask_login import current_user, login_user, login_required, logout_user, LoginManager
+
 # dotenv loader
 from dotenv import load_dotenv
 load_dotenv()
@@ -13,13 +16,43 @@ app = Flask(os.getenv('APP_NAME'))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+
+# login manager
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+        :param unicode user_id: user_id (email) user to retrieve
+    """
+    return User.query.get(user_id)
+
 @app.route('/')
 def home():
     return render_template('auth/login.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user_exist = User.query.filter_by(email=email).first()
+        if not (user_exist and check_password_hash(pwhash=user_exist.password, password=password)):
+            flash('Invalid email or password. Please try again.', 'danger')
+            return render_template('login.html', form=form)
+        
+        db.session.add(user_exist) 
+        db.session.commit()
+        login_user(user_exist, remember=True)
+        flash('Login successfull', 'success')
+        return redirect(url_for('dashboard'))
+    
+    return render_template('auth/login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
