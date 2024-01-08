dd64b1f4aee5aed0c344a6a58da734c0279895cd
abel <ab.bell205@gmail.com>
2024-01-08 21:44:51 +0100

    get todays Event

--- app/file.ps1
+++ app/file.ps1
@@ -4,13 +4,32 @@
 # use the * wildcard
 # Get-EventLog -LogName *
 
-
 #**************** get current log from Application ***************
+# get today event
+$End = Get-Date -Format "MM/dd/yyyy HH:mm:ss" 
+$Begin = Get-Date -format "MM/dd/yyyy 00:00:00"
 
-# $end = Get-Date -Format "dd/MM/yyyy HH:mm:ss" 
-# $start = Get-Date -format "dd/MM/yyyy 00:00:00"
+# $Begin = Get-Date -Date '1/8/2023 08:00:00'
+# $End = Get-Date -Date '1/8/2023 17:00:00'
+# $todayEvents = Get-EventLog -LogName  $args[0] -After $start -Before $End
 
-# $todayEvents = Get-EventLog -LogName Application -After $Begin -Before $End
+$eventName = $args[0]
 
-# convert results to json
-#  
+$todayEvents =  Get-EventLog -LogName $eventName -After $Begin -Before $End | ConvertTo-Json
+
+return $todayEvents
+
+#**************** output path ***************
+# $appLogsCsvPath = "C:\Users\hp\Desktop\Abel\ALx\simple_flask_auth\appLogs.json"
+
+#********* convert results to json *************
+# $todayEvents | ConvertTo-Json | Out-File $appLogsCsvPath
+
+# Get the event specified in the sub process
+
+# Get the current work directory
+# export the result to csv
+# Run save the event to the current directory
+
+# $greeting =  "Hello"
+# return $greeting
