8150e212d1d4629962fea7c8454ad8bdf73955aa
ab-abel <ab.bell205@gmail.com>
2024-01-29 22:49:27 +0100

    PEP8 clean up

--- app/core.py
+++ app/core.py
@@ -7,11 +7,15 @@ import json
 script_path = os.getcwd()+'\\app\\file.ps1'
 
 # run subprocess with the param to generate the file
-json_result = subprocess.run(["powershell.exe", "-File", script_path, 'Security'], stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+json_result = subprocess.run(["powershell.exe",
+                              "-File", script_path,
+                              'setup'],
+                             stdout = subprocess.PIPE, 
+                             stderr = subprocess.PIPE,
+                             universal_newlines = True)
 
 # accept the param from the form
 
-# print(json_result.stdout)
 # print(json_result.stderr)
 
 f = open('app/sample.json', 'w')
