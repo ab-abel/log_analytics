2697652d3f3e4312a1e13585af655f10a12d9463
ab-abel <ab.bell205@gmail.com>
2024-02-27 21:27:50 +0100

    added and event selector test

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,30 @@
-console.log('the Js is added to the page')
+
+
+function select_event(){
+    const select = document.getElementById('selected-category')
+    let selected = ''
+    select.addEventListener('change', (e)=>{  
+        e.preventDefault();
+        
+        if(select.value == 1){
+            selected = 'Application';
+        }
+        else if(select.value == 2){
+            selected = 'Security';
+        }
+        else if(select.value == 3) {
+            selected = 'System';
+        }
+        else if(select.value == 4) {
+            selected = 'Setup';
+        }
+        else if(select.value == 5) {
+            selected = 'Active Directory';
+        }
+        else if(select.value == 6) {
+            selected = 'DNS Server';
+        }
+    })
+    return selected;
+}
+console.log(select_event())
